fa60bccaf1e80c9edecde156d15c1dc6caaa287e
Cristi Tiurbe <cristi@tiurbe.ro>
2015-06-26 13:00:40 +0300

    Inital commit

---
+++ ddostool.rc.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+#
+# chkconfig: - 86 14
+# description: ddos tool daemon
+# processname: ddostool
+# config: /etc/ddostool.conf
+#
+
+### BEGIN INIT INFO
+# Provides: zabbix-agent
+# Required-Start: $local_fs $network
+# Required-Stop: $local_fs $network
+# Should-Start: zabbix zabbix-proxy
+# Should-Stop: zabbix zabbix-proxy
+# Default-Start:
+# Default-Stop: 0 1 2 3 4 5 6
+# Short-Description: Start and stop Zabbix agent
+# Description: Zabbix agent
+### END INIT INFO
+
+# Source function library.
+. /etc/rc.d/init.d/functions
+
+if [ -x /usr/local/bin/ddostool ]; then
+    exec=/usr/local/bin/ddostool
+else
+    exit 5
+fi
+
+prog=${exec##*/}
+conf=/etc/ddostool.conf
+pidfile=$(grep -e "^PidFile=.*$" $conf | cut -d= -f2)
+
+#if [ -f /etc/sysconfig/zabbix-agent ]; then
+#    . /etc/sysconfig/zabbix-agent
+#fi
+
+lockfile=/var/lock/subsys/ddostool
+
+start()
+{
+    echo -n $"Starting DDoS tool: "
+    daemon $exec -c $conf
+    rv=$?
+    echo
+    [ $rv -eq 0 ] && touch $lockfile
+    return $rv
+}
+
+stop()
+{
+    echo -n $"Shutting down DDoS tool: "
+    killproc $prog
+    rv=$?
+    echo
+    [ $rv -eq 0 ] && rm -f $lockfile
+    return $rv
+}
+
+restart()
+{
+    stop
+    start
+}
+
+case "$1" in
+    start|stop|restart)
+        $1
+        ;;
+    force-reload)
+        restart
+        ;;
+    status)
+        status -p $pidfile $prog 
+        ;;
+    try-restart|condrestart)
+        if status $prog >/dev/null ; then
+            restart
+        fi
+        ;;
+    reload)
+        action $"Service ${0##*/} does not support the reload action: " /bin/false
+        exit 3
+        ;;
+    *)
+	echo $"Usage: $0 {start|stop|status|restart|try-restart|force-reload}"
+	exit 2
+	;;
+esac
+
---
+++ ddostool.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+
+. /root/git/ddos/ddostool.conf
+
+#set -x
+
+echoerr() { echo "$@" 1>&2; }
+echolog() { logger -p $DT_LOGFAC -t $DT_LOGTAG "$@"; }
+
+check_binaries()
+{
+  if ! [ -x $IPTABLES ];
+  then
+    echoerr "$IPTABLES not found. Check the config file"
+    exit 1
+  fi
+
+  if ! [ -x $NETSTAT ];
+  then
+    echoerr "$NETSTAT not found. Check the config file"
+    exit 1
+  fi
+
+  if ! [ -x $AWK ];
+  then
+    echoerr "$AWK not found. Check the config file"
+    exit 1
+  fi
+
+  if ! [ -x $CUT ];
+  then
+    echoerr "$CUT not found. Check the config file"
+    exit 1
+  fi
+
+  if ! [ -x $SORT ];
+  then
+    echoerr "$SORT not found. Check the config file"
+    exit 1
+  fi
+
+  if ! [ -x $UNIQ ];
+  then
+    echoerr "$UNIQ not found. Check the config file"
+    exit 1
+  fi
+
+  if ! [ -x $HEAD ];
+  then
+    echoerr "$HEAD not found. Check the config file"
+    exit 1
+  fi
+
+  if ! [ -x $MKTEMP ];
+  then
+    echoerr "$MKTEMP not found. Check the config file"
+    exit 1
+  fi
+
+}
+
+find_bad()
+{
+  $NETSTAT -ntu | $AWK '{print $5}' | $CUT -d: -f1 | $SORT | $UNIQ -c | $SORT -nr > $TMPIPS
+  exec 3<$TMPIPS
+  while read hostconn hostip <&3; do
+    if [ $hostconn -gt $DT_CONN ]; then
+      echo "$hostip" >> $TMPBADIPS
+    fi
+  done
+  exec 3>&-
+  grep -v --file=$DT_WHITELIST $TMPBADIPS > $TMPBANIPS
+}
+
+
+ban_ip()
+{
+  IPTOBAN=$1
+  UTIME=$(date +%s)
+  ban_rule $IPTOBAN
+  echo "$UTIME $IPTOBAN" >> $BANDB
+  echolog "$IPTOBAN was banned."
+}
+
+ban_ips()
+{
+  exec 4<$TMPBANIPS
+  while read hostip <&4; do
+    if [ $(is_banned $hostip) -eq 0 ];
+    then
+      ban_ip $hostip
+    fi
+  done
+  exec 4>&-
+}
+
+unban_ips()
+{
+# Will parse ddosbanned.txtdb and unban expired hosts
+  RIGHTNOW=$(date +%s)
+  TBANDB=$($MKTEMP $DT_TMP/tbandb.XXXXXX)
+  exec 5<$BANDB
+  while read utime hostip <&5; do
+    if [ ! -z $utime ];
+    then
+      if [ $(($RIGHTNOW - $utime)) -gt $DT_TIMEOUT ];
+      then
+        unban_rule $hostip
+      else
+        echo "$UTIME $IPTOBAN" >> $TBANDB
+      fi
+    fi 
+  done
+  exec 5>&-
+  rm -f $BANDB
+  mv $TBANDB $BANDB
+}
+
+ban_rule()
+{
+# Add iptables rule for single ip address
+  IPTOBAN=$1
+  $IPTABLES -A $CHAINNAME -s $IPTOBAN/32 -j REJECT --reject-with icmp-port-unreachable 
+}
+
+unban_rule()
+{
+# Removes rule for single ip address
+  IPTOBAN=$1
+  while [ $(is_banned $IPTOBAN) -eq 1 ]
+  do
+    $IPTABLES -D $CHAINNAME -s $IPTOBAN/32 -j REJECT --reject-with icmp-port-unreachable
+  done
+}
+
+is_banned()
+{
+# Checks if there is a rule for ip in ddostool chain
+  IPTOBAN=$1
+  RULESCOUNT=$($IPTABLES -n --list|grep \ $IPTOBAN\  |wc -l)
+  if [ $RULESCOUNT -gt 0 ];
+  then
+    echo 1
+  else
+    echo 0
+  fi
+}
+
+create_ipt_chain()
+{
+  $IPTABLES -N $CHAINNAME
+  $IPTABLES -I INPUT -p tcp -m multiport --dports 80,443 -j $CHAINNAME
+  $IPTABLES -I $CHAINNAME -j RETURN
+
+}
+
+remove_ipt_chain()
+{
+  $IPTABLES -n --list $CHAINNAME >/dev/null 2>&1
+  if [ $? -eq 0 ];
+  then
+    $IPTABLES -F $CHAINNAME
+    $IPTABLES -D INPUT -p tcp -m multiport --dports 80,443 -j $CHAINNAME
+    $IPTABLES -X $CHAINNAME
+  fi
+}
+# Main stuff
+
+check_binaries
+
+trap "{ echolog \"Exiting\" ; remove_ipt_chain ; exit 0; }" SIGINT SIGTERM
+
+touch $BANDB
+echolog "removing ipt chain"
+remove_ipt_chain
+echolog "creating ipt chain"
+create_ipt_chain
+echolog "starting loop"
+( 
+while [ 1 -eq 1 ]
+do 
+  TMPIPS=$($MKTEMP $DT_TMP/ddosips.XXXXXX)
+  TMPBADIPS=$($MKTEMP $DT_TMP/ddosbadips.XXXXXX)
+  TMPBANIPS=$($MKTEMP $DT_TMP/ddosbanips.XXXXXX)
+  find_bad
+  unban_ips
+  ban_ips
+  rm -f $TMPBADIPS
+  rm -f $TMPIPS
+  rm -f $TMPBANIPS
+  sleep $DT_DELAY
+done 
+) &
