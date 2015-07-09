8d397fe4d7eb829c3bd6cb2fb30233cb2da2cf38
Cristi Tiurbe <cristi@tiurbe.ro>
2015-07-09 14:46:39 +0300

    Added details option to init.d script

--- ddostool.rc.sh
+++ ddostool.rc.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
 #
 # chkconfig: - 86 14
-# description: ddos tool daemon
+# description: ddos prevention tool daemon
 # processname: ddostool
 # config: /etc/ddostool.conf
 #
-
-### BEGIN INIT INFO
-# Provides: zabbix-agent
-# Required-Start: $local_fs $network
-# Required-Stop: $local_fs $network
-# Should-Start: zabbix zabbix-proxy
-# Should-Stop: zabbix zabbix-proxy
-# Default-Start:
-# Default-Stop: 0 1 2 3 4 5 6
-# Short-Description: Start and stop Zabbix agent
-# Description: Zabbix agent
-### END INIT INFO
 
 # Source function library.
 . /etc/rc.d/init.d/functions
@@ -39,7 +27,7 @@ lockfile=/var/lock/subsys/ddostool
 
 start()
 {
-    echo -n $"Starting DDoS tool: "
+    echo -n $"Starting DDoS prevention tool: "
     daemon $exec -c $conf
     rv=$?
     echo
@@ -51,7 +39,7 @@ start()
 
 stop()
 {
-    echo -n $"Shutting down DDoS tool: "
+    echo -n $"Shutting down DDoS prevention tool: "
     killproc $prog
     rv=$?
     echo
@@ -84,8 +72,11 @@ case "$1" in
         action $"Service ${0##*/} does not support the reload action: " /bin/false
         exit 3
         ;;
+    details)
+	$exec -s
+	;;
     *)
-	echo $"Usage: $0 {start|stop|status|restart|try-restart|force-reload}"
+	echo $"Usage: $0 {start|stop|status|restart|try-restart|force-reload|details}"
 	exit 2
 	;;
 esac
